568848e41ec766dd3e939d7efeb8e119026332c2
MarbledNull <cyandoezstuff@gmail.com>
2024-12-01 10:20:19 -0600

    butcher knife, kitchen knife, combat knife

--- src/main/java/net/marblednull/marbledsmelees/init/ModItems.java
+++ src/main/java/net/marblednull/marbledsmelees/init/ModItems.java
@@ -31,6 +31,7 @@ public class ModItems {
     public static final RegistryObject<Item> STEEL_BASEBALL_BAT = ITEMS.register("steel_baseball_bat", () -> new SwordItem(Tiers.IRON, 5, -2.7f, new Item.Properties().durability(750)));
     public static final RegistryObject<Item> KITCHEN_KNIFE = ITEMS.register("kitchen_knife", () -> new SwordItem(Tiers.IRON, 2, -2.7f, new Item.Properties().durability(125)));
     public static final RegistryObject<Item> BUTCHER_KNIFE = ITEMS.register("butcher_knife", () -> new SwordItem(Tiers.IRON, 3, -2.7f, new Item.Properties().durability(125)));
+    public static final RegistryObject<Item> COMBAT_KNIFE = ITEMS.register("combat_knife", () -> new SwordItem(Tiers.IRON, 2, -1.4f, new Item.Properties().durability(200)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
